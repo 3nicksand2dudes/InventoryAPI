2393cd1a397056dcc9870a4cb3abc10f408e7b49
harjunpnik <niklasharjunpaa@gmail.com>
2020-02-28 21:42:17 +0200

    Made own check for duplicate entries

--- inventory/createInAllSites.js
+++ inventory/createInAllSites.js
@@ -35,7 +35,7 @@ module.exports.createInAllSites = (event, context, callback) => {
         if (err) {
             console.error(err)
             callback(null, {
-              statusCode: err.statusCode || 501,
+              statusCode: err.statusCode || 500,
               headers: { 'Content-Type': 'text/plain' },
               body: 'Couldn\'t create the inventory item.',
             })
@@ -43,9 +43,8 @@ module.exports.createInAllSites = (event, context, callback) => {
 
         // No sites exist
         }else if(result.Items.length < 1){
-            console.error("Here2")
             callback(null, {
-              statusCode: err.statusCode || 501,
+              statusCode: err.statusCode || 500,
               headers: { 'Content-Type': 'text/plain' },
               body: 'Couldn\'t create the inventory item. No sites exists',
             })
@@ -55,59 +54,74 @@ module.exports.createInAllSites = (event, context, callback) => {
 
             let failedItems = []
 
-            // TODO Dont allow duplicates
+            // Scan inventory table and save result as obj for later use to check for duplicate entries
+            const scanParams = {
+                TableName: INVENTORY_TABLE,
+                ProjectionExpression: "modelNumber, site",
+            }
+            let existingEntries
+            dynamoDb.scan(scanParams, (error, res) => {
+                existingEntries = res.Items
 
-            // Add item to each site with inventory 0 
-            result.Items.forEach(element => {
+                // Add item to each site with inventory 0 
+                result.Items.forEach(element => {
+                    let entryExists = false
 
-                let params = {
-                    TableName: INVENTORY_TABLE,
-                    Item: {
-                      id: uuid(),
-                      modelNumber: data.modelNumber,
-                      amount: 0,
-                      site: element.siteId,
-                      deleted: false,
-                    },
-                    ConditionExpression: 'attribute_not_exists(siteId)'
-                }
-            
-                dynamoDb.put(params, (error) => {
-                    // If it fails, extend array of failed
-                    if (error) {
-                      console.error(error)
-                      failedItems.push(params)
-                    }
+                    existingEntries.forEach(e =>{
+                        if(e.modelNumber ==  data.modelNumber && e.site == element.siteId){
+                            entryExists = true
+                        }
+                    })
+
+                    // if entry does not exist
+                    if(!entryExists){
+
+                        let params = {
+                            TableName: INVENTORY_TABLE,
+                            Item: {
+                            id: uuid(),
+                            modelNumber: data.modelNumber,
+                            amount: 0,
+                            site: element.siteId,
+                            deleted: false,
+                            },
+                            //ConditionExpression: 'attribute_not_exists(modelNumber) and attribute_not_exists(site) ' // Should work, does not
+                        }
+                    
+                        dynamoDb.put(params, (error) => {
+                            // If it fails, extend array of failed
+                            if (error) {
+                            console.error(error)
+                            failedItems.push(params)
+                            }
+
+                        })
+                    }else{
+                        failedItems.push(data.modelNumber)
+                    }   
 
                 })
-                
-                // If any has failed, report it back
-                if(failedItems.length > 0){
-                     const response = {
-                         statusCode: 501,
-                         headers: { 'Content-Type': 'text/plain' },
-                         body: 'Couldn\'t create item created to every site' ,
-                     }
-                 callback(null, response)
-
-                }
-                // if all succeded
-                else{
-                     const response = {
-                         statusCode: 201,
-                         headers: { 'Content-Type': 'text/plain' },
-                         body: 'Item created to every site',
-                     }
-                 callback(null, response)
-                }
-
-                
             })
 
+            // If any has failed, report it back
+            if(failedItems.length > 0){
+                const response = {
+                    statusCode: 202,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Couldn\'t create item created to every site' ,
+                }
+            callback(null, response)
 
+           }
+           // if all succeded
+           else{
+                const response = {
+                    statusCode: 201,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Item created to every site',
+                }
+            callback(null, response)
+           }
         }
-
     })
-  
-    
 }
